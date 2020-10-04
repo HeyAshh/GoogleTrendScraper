a15c914a1f921fbc123673157b794d336e1380bd
Ashh <Heyashh1@gmail.com>
2020-10-05 00:51:52 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fetch = require('node-fetch');
 
 (async function(){
-    const res = await fetch('https://news.ycombinator.com/');
+    const res = await fetch('https://explodingtopics.com/featured-topics-this-month');
     const text = await res.text();
+    const found = text.toLowerCase().includes('ring lights')
     console.log({text});
 })();
